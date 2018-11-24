230fdd9df5f24fcf48d47a64d9214b7aabf7fa3f
Maciej Szumowski <mpszumowski@gmail.com>
2018-11-24 19:34:20 +0100

    reformat options argument for connexion v. 2.0

--- serve.py
+++ serve.py
@@ -7,7 +7,9 @@ from info import __version__, __title__
 connexion_app = connexion.FlaskApp(
     __name__,
     specification_dir='.',
-    swagger_ui=True,
+    options={
+        "swagger_ui": True
+    },
     arguments={
         'title': __title__,
         'version': __version__
@@ -17,6 +19,7 @@ connexion_app.add_api('swagger.yaml')
 app = connexion_app.app
 
 if __name__ == "__main__":
+    # TODO: move address to config
     bind_address = '0.0.0.0'
     port = 8000
     server = Server((bind_address, port), app)
