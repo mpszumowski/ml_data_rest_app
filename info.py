232973ce1617f132b7c1b098ac1ad50c94ad2c55
Maciej Szumowski <mpszumowski@gmail.com>
2018-11-24 19:20:23 +0100

    add app info

---
+++ info.py
@@ -0,0 +1,6 @@
+__title__ = 'webfetch'
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+MICRO_VERSION = 0
+__version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+__author__ = 'Maciej Szumowski'
