532c4be5e70833d47a0d9c43eb291b77d938ea9e
Maciej Szumowski <mpszumowski@gmail.com>
2018-12-01 21:31:01 +0100

    download content tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,6 @@
-import pytest
 from unittest import mock
 
-from ml_data import *
+from ml_data import api, serve
 
 
 @mock.patch('ml_data.api.get_images')
@@ -23,13 +22,32 @@ def test_fetch_text(mock_get_text):
 
 
 @mock.patch('ml_data.api._gen_image_fp')
-def test_download_images(_gen_image_fp):
-    pass
+def test_download_images(mock__gen_image_fp):
+    with serve.app.test_request_context('/ml/content/images'):
+        response = api.download_images()
+    assert response.mimetype == 'application/zip'
+    assert response.status_code == 200
 
 
-def test_download_text():
-    pass
+@mock.patch('ml_data.api._gen_text_data')
+def test_download_text(mock__gen_text_data):
+    with serve.app.test_request_context('/ml/content/text'):
+        response = api.download_text()
+    assert response.mimetype == 'text/plain'
+    assert response.status_code == 200
 
 
-def test_download_all():
-    pass
+@mock.patch('ml_data.api._gen_text_data')
+@mock.patch('ml_data.api._gen_image_fp')
+def test_download_all(mock__gen_image_fp, mock__gen_text_data):
+    mock__gen_text_data().getvalue.return_value = b'image'
+    mock__gen_image_fp.result_value = b'f_path', b'f_name'
+    with serve.app.test_request_context('/ml/content'):
+        response = api.download_all()
+    assert response.mimetype == 'application/zip'
+    assert response.status_code == 200
+
+
+def test_get_status():
+    api.c_app.conf.task_always_eager = True
+
